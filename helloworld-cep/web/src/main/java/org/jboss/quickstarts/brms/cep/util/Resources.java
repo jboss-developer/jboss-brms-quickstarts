6ad9971d28846555218eace015d7f1cfc2e07d1e
Rafael Benevides <rafabene@gmail.com>
2014-02-03 17:33:41 -0200

    JDF-586 Create Intermediate Quickstarts - helloword-cep

---
+++ helloworld-cep/web/src/main/java/org/jboss/quickstarts/brms/cep/util/Resources.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.quickstarts.brms.cep.util;
+
+import javax.enterprise.inject.Produces;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.inject.Named;
+
+import org.jboss.quickstarts.brms.cep.model.TransactionType;
+
+/**
+ * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
+ * 
+ * <p>
+ * Example injection on a managed bean field:
+ * </p>
+ * 
+ * <pre>
+ * &#064;Inject
+ * private EntityManager em;
+ * </pre>
+ */
+public class Resources {
+
+    @Produces
+    @Named("transactionsType")
+    public SelectItem[] getTransactionsType() {
+        SelectItem[] items = new SelectItem[TransactionType.values().length];
+        int i = 0;
+        for (TransactionType type : TransactionType.values()) {
+            items[i] = new SelectItem(type, type.name());
+            i++;
+        }
+        return items;
+    }
+    
+    @Produces
+    public FacesContext getFacesContext(){
+        return FacesContext.getCurrentInstance();
+    }
+
+}
